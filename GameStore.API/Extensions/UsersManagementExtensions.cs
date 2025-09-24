eaadfd437c46b3993cdb3ef57c8a4a730a474d02
Сергій <sergvoy1996@gmail.com>
2025-09-24 14:18:07 +0300

    fix identity db connection error

--- GameStore.API/Extensions/UsersManagementExtensions.cs
+++ GameStore.API/Extensions/UsersManagementExtensions.cs
@@ -10,7 +10,12 @@ public static class UsersManagementExtensions
     public static IServiceCollection AddUserManagement(this IServiceCollection services, IConfiguration config)
     {
         services.AddDbContext<AuthDbContext>(options =>
-            options.UseSqlServer(config.GetConnectionString("IdentityDb")));
+            options.UseSqlServer(
+                config.GetConnectionString("IdentityDb"),
+                sqlOptions => sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorNumbersToAdd: null)));
         services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<AuthDbContext>()
             .AddDefaultTokenProviders();
